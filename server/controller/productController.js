31b5d883ad0e550bfb2308e4aea554372fef3eb2
MattyHup6434 <122677300+MattyHup6434@users.noreply.github.com>
2025-07-05 21:09:09 +0700

    update post and put

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -2,11 +2,12 @@ const prisma = require('../prisma/prisma')
 const admin = "matty";
 exports.postProduct = async (req, res) => {
     try {
-        const { name, price, stock } = req.body;
-        const newProduct = await prisma.product.create({    
+        const { name, price, stock, description } = req.body;
+        const newProduct = await prisma.product.create({
             data: {
                 name: name,
                 price: price,
+                description: description,
                 stock: stock,
                 creatBy: admin,
                 updatedBy: admin
@@ -16,26 +17,26 @@ exports.postProduct = async (req, res) => {
         res.json(newProduct);
     } catch (err) {
         console.error(err);
-         res.status(500).json({ error: 'Internal Server Error' });
-        
+        res.status(500).json({ error: 'Internal Server Error' });
+
     }
 
 }
 exports.getProduct = async (req, res) => {
     try {
         const products = await prisma.product.findMany()
-         if (!products) {
+        if (!products) {
             return res.status(404).send("Product not found");
         }
         res.json(products);
     } catch (err) {
         console.error(err);
-           res.status(500).json({ error: 'Internal Server Error' });
+        res.status(500).json({ error: 'Internal Server Error' });
     }
 }
 exports.getProductByid = async (req, res) => {
     try {
-        const {productid} = req.params
+        const { productid } = req.params
         const product = await prisma.product.findUnique({
             where: {
                 id: parseInt(productid)
@@ -47,41 +48,42 @@ exports.getProductByid = async (req, res) => {
         res.json(product);
     } catch (err) {
         console.error(err);
-           res.status(500).json({ error: 'Internal Server Error' });
+        res.status(500).json({ error: 'Internal Server Error' });
     }
 }
 exports.putProductByid = async (req, res) => {
     try {
-     const { putproductid } = req.params;
-    //  console.log("putproductid:", putproductid);
-     const { name, price, stock } = req.body; 
-       
-     const updatedProduct = await prisma.product.update({
-            where:{
-               id: parseInt(putproductid)
+        const { putproductid } = req.params;
+        //  console.log("putproductid:", putproductid);
+        const { name, price, stock, description } = req.body;
+
+        const updatedProduct = await prisma.product.update({
+            where: {
+                id: parseInt(putproductid)
             },
-        data:{
+            data: {
                 name: name,
                 price: price,
                 stock: stock,
+                description: description,
                 updatedBy: admin
-        }
+            }
         })
         res.json(updatedProduct);
     } catch (err) {
         console.error(err);
-           res.status(500).json({ error: 'Internal Server Error' });
+        res.status(500).json({ error: 'Internal Server Error' });
     }
 }
 exports.deleteProductByid = async (req, res) => {
     try {
-        const { deleteProductid } = req.params; 
+        const { deleteProductid } = req.params;
         const product = await prisma.product.delete({
             where: {
                 id: parseInt(deleteProductid)
             }
-        }); 
-            res.json({
+        });
+        res.json({
             message: 'successfully deleted',
             product: product
         });
